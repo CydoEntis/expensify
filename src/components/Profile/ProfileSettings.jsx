89e370b9fa97288451c8c28f1b244bed48f111ca
Cody Stine <cydoentis@gmail.com>
2021-09-17 20:05:31 -0400

    Added settings skeleton

--- src/components/Profile/ProfileSettings.jsx
+++ src/components/Profile/ProfileSettings.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import Modal from "../UI/Modals/Modal";
+import Button from "../UI/Buttons/Button";
 
-const ProfileSettings = () => {
-	return <div></div>;
+import styles from "./ProfileSettings.module.css";
+
+const ProfileSettings = (props) => {
+	return (
+		<Modal onClose={props.onClose}>
+			<h1>Settings</h1>
+			<form>
+				{/* {selectionHasError && <p>Please select a type.</p>} */}
+				<input
+					type="text"
+					placeholder="Name"
+					// onChange={}
+					// onBlur={}
+					// value={}
+				/>
+				{/* {nameHasError && <p>Please don't leave name empty.</p>} */}
+				<input
+					type="text"
+					placeholder="Cost"
+					// onChange={}
+					// onBlur={}
+					// value={}
+				/>
+				<div>
+					<Button type="button" onClick={props.onClose}>
+						Cancel
+					</Button>
+					<Button type="submit">Add Expense</Button>
+				</div>
+			</form>
+		</Modal>
+	);
 };
 
 export default ProfileSettings;
